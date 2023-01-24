6749b37c3dca63206e3f8d7d56e594ab369ca423
Karol <k.kalist928@gmail.com>
2023-01-24 08:38:29 +0100

    waveshare epd configuration

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python3
+
+from lib import epd4in2
+from PIL import Image, ImageDraw,ImageFont
+from datetime import datetime
+
+import RPi._GPIO as GPIO
 import bme280
 import smbus2
 import os
+import json
 import time
 
-from lib.waveshare_epd import epd4in2
-from PIL import Image, ImageDraw,ImageFont
+RES_FONT = os.path.join(os.path.dirname(os.path.relpath(__file__)),'font/')
+RES_PIC = os.path.join(os.path.dirname(os.path.relpath(__file__)),'pic/')
 
+DATE_FORMAT = "%b %d %Y"
+TIME_FORMAT = ""
 
-
+x = datetime(2022, 9, 15, 12, 45, 35)
+print(x.strftime("%b %d %Y %H:%M:%S"))
 
 
 '''
